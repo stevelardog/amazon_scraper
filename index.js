95759636fbd332b0681e685a3b5f0247cf89f7f5
stevelardog <34727980+stevelardog@users.noreply.github.com>
2022-11-27 09:08:23 -0600

    main

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const request = require('request-promise');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+// PUT SCRAPER API KEY HERE vvvvvvvvvvvvvvvvv  https://dashboard.scraperapi.com/
+const apiKey = '';
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+
+app.use(express.json()); //allow app to parse json
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Amazon Scraper API')
+})
+
+//GET product details
+app.get('/products/:productId', async (req, res) =>{
+    const{productId} = req.params;
+
+    try {
+            const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`);
+
+            res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+})
+
+//GET product Price
+app.get('/products/:productId/reviews', async (req, res) =>{
+    const{productId} = req.params;
+
+    try {
+            const response = await request(`${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`);
+
+            res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+})
+
+//GET product Offers
+app.get('/products/:productId/offers', async (req, res) =>{
+    const{productId} = req.params;
+
+    try {
+            const response = await request(`${baseUrl}&url=https://www.amazon.com/gp/offer-listing${productId}`);
+
+            res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+})
+
+//GET search results
+app.get('/search/:searchQuery', async (req, res) =>{
+    const{searchQuery} = req.params;
+
+    try {
+            const response = await request(`${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`);
+
+            res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
